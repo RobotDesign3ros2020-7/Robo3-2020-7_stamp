b694757811618a3d2485fddd5eb91a94101fe696
TakumiKawamoto <s19c1033ap@s.chibakoudai.jp>
2020-11-02 15:16:11 +0900

    Add a hand angle2

--- crane_x7_examples/scripts/stamp_demo.py
+++ crane_x7_examples/scripts/stamp_demo.py
@@ -102,9 +102,9 @@ def main():
     target_pose(0.3, 0.1, PICK_Z-0.02)
     
     # 角度変更
-    target_joint_values(8, -0.785398)
+    target_joint_values(5, -0.785398)
     rospy.sleep(1.0)
-#    target_joint_values(6, -10)
+    target_joint_values(5, -0.785398)
     rospy.sleep(1.0)
     
     # ハンドを持ち上げる
