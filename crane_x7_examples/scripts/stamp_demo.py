41e02bc488957d99211f74f958b0ed5cad642a41
TakumiKawamoto <s19c1033ap@s.chibakoudai.jp>
2020-11-09 16:04:38 +0900

    シャチハタ用に変更

--- crane_x7_examples/scripts/stamp_demo.py
+++ crane_x7_examples/scripts/stamp_demo.py
@@ -73,30 +73,33 @@ def main():
     target_pose(0.35, 0.13, PICK_Z)
 
     # ハンドを閉じる
-    gripper.set_joint_value_target([0.05, 0.05])
+    gripper.set_joint_value_target([0.1, 0.1])
     gripper.go()
 
     # 持ち上げる
     target_pose(0.35, 0.13, 0.3)
 
     # 移動する 朱肉まで
-    target_pose(0.35, -0.11, 0.3)
+#    target_pose(0.35, -0.11, 0.3)
 
     # 下ろす 朱肉をつける
-    target_pose(0.35, -0.11, PICK_Z)
+#    target_pose(0.35, -0.11, PICK_Z)
 
     # 少しだけハンドを持ち上げる
-    target_pose(0.35, -0.11, 0.15)
+#    target_pose(0.35, -0.11, 0.15)
     
     # 下ろす 朱肉をつける2回目
-    target_pose(0.35, -0.11, PICK_Z)
+#    target_pose(0.35, -0.11, PICK_Z)
     
     # ハンドを持ち上げる
-    target_pose(0.35, -0.11, 0.3)
+#    target_pose(0.35, -0.11, 0.3)
     
     # 移動する 捺印場所
     target_pose(0.2, 0.0, 0.3)
     rospy.sleep(1.0)
+    
+    # 捺印の準備をする
+    target_pose(0.2, 0.0, 0.16)
     
     # 下ろす 捺印
     target_pose(0.2, 0.0, PICK_Z)
@@ -113,17 +116,17 @@ def main():
     rospy.sleep(1.0)
     
     # 移動する ホームポジション
-    target_pose(0.2, 0.0, 0.3)
+    target_pose(0.35, 0.13, 0.3)
     
     # 下ろす ホームポジション
-    target_pose(0.2, 0.0, PICK_Z)
+    target_pose(0.35, 0.13, PICK_Z)
     
     # ハンドを開く
     gripper.set_joint_value_target([0.7, 0.7])
     gripper.go()
     
     # 少しだけハンドを持ち上げる
-    target_pose(0.2, 0.0, 0.2)
+    target_pose(0.35, 0.13, 0.2)
 
     # SRDFに定義されている"home"の姿勢にする
     arm.set_named_target("home")
