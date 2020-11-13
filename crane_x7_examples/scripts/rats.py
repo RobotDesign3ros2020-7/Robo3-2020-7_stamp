2c24cdf256ffa272b794a25dc947e99e93001c21
HoshinoMasafumi <s19C1105lW@s.chibakoudai.jp>
2020-11-13 14:23:45 +0900

    add

--- crane_x7_examples/scripts/rats.py
+++ crane_x7_examples/scripts/rats.py
@@ -12,7 +12,7 @@ def main():
     rospy.init_node("pose_groupstate_example")
     robot = moveit_commander.RobotCommander()
     arm = moveit_commander.MoveGroupCommander("arm")
-    arm.set_max_velocity_scaling_factor(0.1)
+    arm.set_max_velocity_scaling_factor(0.75)
     gripper = moveit_commander.MoveGroupCommander("gripper")
 
     while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
@@ -65,24 +65,48 @@ def main():
         arm.set_pose_target( target_pose )	# 目標ポーズ設定
         arm.go()				# 実行
 
+    def set_pos_1(set_x,set_y,set_z):
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = set_x
+        target_pose.position.y = set_y
+        target_pose.position.z = set_z
+        q = quaternion_from_euler( -3.14/2.0, 0.0, -3.14/2.0 )
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target( target_pose )	# 目標ポーズ設定
+        arm.go()				# 実行
+
+
     home_pos()
 
     set_pos(0.33,0.,0.23)
-    set_pos(0.33,0.15,0.12)
+    set_pos(0.33,0.15,0.23)
+    set_pos(0.33,0.15,0.13)
 
-    open_close(0.2)
+    open_close(0.1)
 
+    set_pos(0.33,0.15,0.23)
     set_pos(0.33,0.,0.23)
     set_pos(0.33,-0.15,0.23)
-    set_pos(0.33,-0.15,0.12)
+    set_pos(0.33,-0.15,0.13)
     set_pos(0.33,-0.15,0.18)
     set_pos(0.33,-0.15,0.13)
     set_pos(0.33,-0.15,0.18)
 
     set_pos(0.22,0.,0.18)
     set_pos(0.22,0.,0.13)
+    set_pos(0.20,0.,0.13)
+    set_pos(0.20,0.,0.11)
+    set_pos(0.20,0.01,0.11)
     set_pos(0.22,0.,0.18)
 
+    set_pos(0.33,0.15,0.18)
+    set_pos(0.33,0.15,0.13)
+
+
+    open_close(0.7)
 
     home_pos()
     vertical_pos()
