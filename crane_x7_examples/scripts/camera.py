e729decbbbe098e077f9432bffcc36e6242ea36a
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-07 13:33:41 +0900

    Update camera.py

--- crane_x7_examples/scripts/camera.py
+++ crane_x7_examples/scripts/camera.py
@@ -8,11 +8,11 @@ import cv2
 from sensor_msgs.msg import Image 
 from cv_bridge import CvBridge
 
-def process_image(msg):
+def process_image(msg):                                 #画像処理
     try:
         bridge = CvBridge()
         orig = bridge.imgmsg_to_cv2(msg, "bgr8")
-        img = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)    
         cv2.imshow('image', img)
         cv2.waitKey(1)
     except Exception as err:
